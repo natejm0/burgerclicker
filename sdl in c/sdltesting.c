3885f797cffb56427067584a3ab17fe979aab1c1
NathanJM <144484634+natejm0@users.noreply.github.com>
2023-09-26 13:52:02 -0400

    added burger

--- sdl in c/sdltesting.c
+++ sdl in c/sdltesting.c
@@ -1,14 +1,27 @@
 #define SDL_MAIN_HANDLED
+#define ASSETS_PATH "..\\assets\\"
+#define SPRITES_PATH "sprites\\"
+#define BURGER_SPRITE ASSETS_PATH SPRITES_PATH "burger.png"
 #include <SDL.h>
+#include <SDL_image.h>
 #include <stdio.h>
 
 int main(int argc, char* argv[]) {
+    
+    SDL_Texture *imageTexture = NULL;
+    SDL_Window *window = NULL;
+    SDL_Renderer *renderer = NULL;
+
     if (SDL_Init(SDL_INIT_EVERYTHING) < 0) {
         printf("SDL init failed: %s\n", SDL_GetError());
         return 1;
     }
+    if(!(IMG_Init(IMG_INIT_PNG) & IMG_INIT_PNG)) {
+        printf("SDL_image could not initialize! SDL_image Error: %s\n", IMG_GetError());
+        return 1;
+    }
 
-    SDL_Window *window = SDL_CreateWindow(
+    window = SDL_CreateWindow(
         "Burger Clicker",
         SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED,
         800, 700,
@@ -20,25 +33,78 @@ int main(int argc, char* argv[]) {
         return 1;
     }
     
-    SDL_Renderer *renderer = SDL_CreateRenderer( window, -1, 0 );
+    // Create renderer
+    renderer = SDL_CreateRenderer( window, -1, SDL_RENDERER_ACCELERATED );
 
-    SDL_SetRenderDrawColor( renderer, 255, 0, 0, 255 );
+    if (renderer == 0) {
+        printf("Failed to load renderer");
+        goto cleanup;
+    }
 
-    SDL_RenderClear( renderer );
+    // Set BG Color
+    SDL_SetRenderDrawColor( renderer, 204, 255, 229, 255 );
 
     SDL_Event event;
 
-    while(1 == 1) {
+    printf(BURGER_SPRITE);
+    SDL_Surface *loadedSurface = IMG_Load(BURGER_SPRITE);
+    
+
+
+    if (loadedSurface == 0) {
+        printf("Failed to load burger sprite");
+        goto cleanup;
+    }
+
+    imageTexture = SDL_CreateTextureFromSurface(renderer, loadedSurface);
+    if (!imageTexture) {
+        printf("Failed to create texture");
+        goto cleanup;
+    }    
+    
+    SDL_FreeSurface(loadedSurface);
+
+    int gameIsRunning = 1;
+    
+    int image_width, image_height;
+    SDL_QueryTexture(imageTexture, NULL, NULL, &image_width, &image_height);
+    
+    SDL_Rect rect;
+    rect.x = 400-128;
+    rect.y = 350-128;
+    rect.w = 256;
+    rect.h = 256;
+
+    while(gameIsRunning) {
         if ( SDL_PollEvent( &event ) ) {
             if (SDL_KEYDOWN == event.type) {
                 if (SDLK_ESCAPE == event.key.keysym.sym) {
-                    SDL_DestroyWindow(window);
-                    SDL_Quit();
+                    gameIsRunning = 0;
                 }
             }
-    
         }
+        
+        SDL_RenderClear( renderer );
+        
+        SDL_RenderCopy(renderer, imageTexture, NULL, &rect); // Render the whole texture to the screen
+        
+        SDL_RenderPresent(renderer);
     }
+
+cleanup:
+    
+    if (imageTexture) {
+        SDL_DestroyTexture(imageTexture);
+    }
+    if (renderer) {
+        
+        SDL_DestroyRenderer(renderer);
+    }
+    
+    SDL_DestroyWindow(window);
+    
+    IMG_Quit();
+    SDL_Quit();
 
     return 0;
 }
